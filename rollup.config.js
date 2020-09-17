e7797127cd6eac5578e29cbea8d9a82a854f8d81
myWsq <wsq961@outlook.com>
2020-09-17 11:04:39 +0800

    add style

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,29 @@
 import dts from "rollup-plugin-dts";
-
-module.exports = {
-  input: "src/index.ts",
-  output: [
-    {
-      format: "es",
-      file: `dist/index.d.ts`,
-    },
-  ],
-
-  external: process.env.BUILD === "external" ? [/\.scss$/] : [],
-  plugins: [dts()],
-};
+import postcss from "rollup-plugin-postcss";
+module.exports = [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        format: "esm",
+        file: `dist/index.js`,
+      },
+    ],
+    plugins: [
+      postcss({
+        extract: true,
+      }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        format: "es",
+        file: `dist/index.d.ts`,
+      },
+    ],
+    external: process.env.BUILD === "external" ? [/\.scss$/] : [],
+    plugins: [dts()],
+  },
+];
